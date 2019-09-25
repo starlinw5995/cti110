3767413b043f275d6de4223e729d4601b9884c64
starlinw5995 <55775432+starlinw5995@users.noreply.github.com>
2019-09-24 23:31:46 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_WilliamStarling.py
@@ -0,0 +1,12 @@
+# Projected amount of total sales.
+# 09-20-19
+# CTI-110 P2T1 - Sales Prediction
+# William Starling
+#
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
