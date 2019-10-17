e173b5e87803f566e8f1c45016d29b200d3fc344
starlinw5995 <55775432+starlinw5995@users.noreply.github.com>
2019-10-16 22:41:53 -0400

    Add files via upload
    Initials

---
+++ P4T1b_Starling_py.py
@@ -0,0 +1,74 @@
+# CTI-110
+# P4T1b: Initials
+# William Starling
+# 10/15/2019
+#
+# Import Turtle.
+import turtle
+
+# Hide the turtle arrow.
+turtle.hideturtle()
+
+# Pick pen up.
+turtle.penup()
+
+# Go to X and Y coordinates of (-250,250).
+turtle.goto(-250,250)
+
+# Put the Pen down to draw. 
+turtle.pendown()
+
+# Set pen size as 5.
+turtle.pensize(5)
+
+# Set pen color as sky blue.
+turtle.pencolor('sky blue')
+
+# Turn right 70 degrees.
+turtle.right(70)
+
+# Go forward 150 pixels.
+turtle.forward(150)
+
+# Turn left 140 degrees.
+turtle.left(140)
+
+# Go forward 150 pixels.
+turtle.forward(150)
+
+# Turn right 140 degrees.
+turtle.right(140)
+
+# Go forward 150 pixels.
+turtle.forward(150)
+
+# Turn left 140 degrees.
+turtle.left(140)
+
+# Go forward 150 pixels.
+turtle.forward(150)
+
+# Pick pen up.
+turtle.penup()
+
+# Go to X and Y coordinates of (150,225).
+turtle.goto(100,225)
+
+# Put the Pen down to draw.
+turtle.pendown()
+
+# Set pen color as dark blue.
+turtle.pencolor('dark blue')
+
+# Turn left 50 degrees.
+turtle.left(50)
+
+# Draw a circle with a radius of 30 and 240 degress around.
+turtle.circle(30,240)
+
+# Turn right 180 degrees.
+turtle.right(180)
+
+# Draw a circle with a radius of 30 and -240 degress around.
+turtle.circle(30,-240)
+
